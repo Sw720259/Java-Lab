a264f3cb174931f15229a1247d68579654f438f7
Sw720259 <swathi.v1@s.amity.edu>
2026-06-18 19:16:15 +0530

    To print Input Different Data Types Using Scanner

---
+++ scanner.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class InputDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Integer: ");
+        int i = sc.nextInt();
+
+        System.out.print("Enter Float: ");
+        float f = sc.nextFloat();
+
+        System.out.print("Enter Double: ");
+        double d = sc.nextDouble();
+
+        System.out.print("Enter Character: ");
+        char c = sc.next().charAt(0);
+
+        System.out.print("Enter Boolean: ");
+        boolean b = sc.nextBoolean();
+
+        sc.nextLine(); // consume newline
+        System.out.print("Enter String: ");
+        String s = sc.nextLine();
+
+        System.out.println("\n--- Entered Values ---");
+        System.out.println("Integer: " + i);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + b);
+        System.out.println("String: " + s);
+    }
+}
+
