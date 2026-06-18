2db6b47e2cc59598cf6ded515df6d621648850bd
Sw720259 <swathi.v1@s.amity.edu>
2026-06-18 18:54:35 +0530

    Frequency of Each Element using Array

---
+++ arrfreq.java
@@ -0,0 +1,20 @@
+public class ArrayExample5 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 1, 4, 2};
+        boolean[] visited = new boolean[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!visited[i]) {
+                int count = 1;
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i] == arr[j]) {
+                        visited[j] = true;
+                        count++;
+                    }
+                }
+                System.out.println(arr[i] + " occurs " + count + " times");
+            }
+        }
+    }
+}
+
