5b93bd9462850ae15656a57ba7889e91378b1eb3
Sw720259 <swathi.v1@s.amity.edu>
2026-06-19 11:41:56 +0530

    Creating a Finally Block

---
+++ final.java
@@ -0,0 +1,13 @@
+public class DivisionFinally {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero!");
+        } finally {
+            System.out.println("This message always prints.");
+        }
+    }
+}
+
