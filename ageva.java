e3c5bfca45093737b4cde3fa7d422277b798dee6
Sw720259 <swathi.v1@s.amity.edu>
2026-06-19 11:42:58 +0530

    Creating a Age validation

---
+++ ageva.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AgeValidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        try {
+            if (age < 18) throw new Exception("Not eligible to vote!");
+            System.out.println("Eligible to Vote");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
