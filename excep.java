b09edf04f6089e8264da518749354394e3672f31
Sw720259 <swathi.v1@s.amity.edu>
2026-06-19 11:47:56 +0530

    Creating a Exception Propagation

---
+++ excep.java
@@ -0,0 +1,16 @@
+public class Propagation {
+    static void method1() {
+        int a = 10 / 0;
+    }
+    static void method2() {
+        method1();
+    }
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled in main: " + e);
+        }
+    }
+}
+
