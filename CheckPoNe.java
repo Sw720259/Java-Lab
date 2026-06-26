dad000341164b611f39512c6f375d7274d71afb8
Sw720259 <swathi.v1@s.amity.edu>
2026-06-26 21:57:04 +0530

    Create a  class to check no is Positiove or Negative or Zero

---
+++ CheckPoNe.java
@@ -0,0 +1,14 @@
+public class CheckNumber {
+    public static void main(String[] args) {
+        int num = -15;  // you can change this value
+
+        if (num > 0) {
+            System.out.println(num + " is Positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is Negative.");
+        } else {
+            System.out.println("The number is Zero.");
+        }
+    }
+}
+
